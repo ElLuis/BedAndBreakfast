479d8b578a382dd2b0adab08f61f26fe911ff82d
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2017-08-13 20:19:46 -0400

    Added two labels. Getting familiar with the autogenerated  code

--- BedAndBreakfast/Form1.Designer.cs
+++ BedAndBreakfast/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BedAndBreakfast
 {
-    partial class Form1
+    partial class BaileysForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.welcomeLabel1 = new System.Windows.Forms.Label();
+            this.rateLabel1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeLabel1
+            // 
+            this.welcomeLabel1.AutoSize = true;
+            this.welcomeLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel1.Location = new System.Drawing.Point(20, 30);
+            this.welcomeLabel1.Name = "welcomeLabel1";
+            this.welcomeLabel1.Size = new System.Drawing.Size(248, 29);
+            this.welcomeLabel1.TabIndex = 0;
+            this.welcomeLabel1.Text = "Welcome to Bailey\'s";
+            // 
+            // rateLabel1
+            // 
+            this.rateLabel1.AutoSize = true;
+            this.rateLabel1.Location = new System.Drawing.Point(112, 86);
+            this.rateLabel1.Name = "rateLabel1";
+            this.rateLabel1.Size = new System.Drawing.Size(82, 13);
+            this.rateLabel1.TabIndex = 1;
+            this.rateLabel1.Text = "Check our rates";
+            // 
+            // BaileysForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(429, 364);
+            this.Controls.Add(this.rateLabel1);
+            this.Controls.Add(this.welcomeLabel1);
+            this.Name = "BaileysForm";
+            this.Text = "Bailey\'s Bed and Breakfast";
+            this.Load += new System.EventHandler(this.BaileysForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label welcomeLabel1;
+        private System.Windows.Forms.Label rateLabel1;
     }
 }
 
--- BedAndBreakfast/Form1.cs
+++ BedAndBreakfast/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace BedAndBreakfast
 {
-    public partial class Form1 : Form
+    public partial class BaileysForm : Form
     {
-        public Form1()
+        public BaileysForm()
         {
             InitializeComponent();
+        }
+
+        private void BaileysForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
